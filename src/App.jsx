e264e910866134f6a7347563ea2a10660ccb79bc
ABDE-SDK <abderrahmane.sidki@gmail.com>
2026-03-14 14:50:12 +0000

    chore: setup Tailwind CSS v4, React Router, Recharts and project structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,12 @@
 function App() {
-  return("")
+  return (
+    <div className="flex h-screen items-center justify-center bg-slate-900">
+      <div className="bg-orange-500 p-10 rounded-2xl shadow-2xl">
+        <h1 className="text-4xl font-black text-white italic ">
+          TAILWIND IS WORKING! 🔥
+        </h1>
+      </div>
+    </div>
+  )
 }
-
 export default App
