f82982a4157069ce2a0a72d45dff89dd604791de
aitreda206 <2006112500157@ofppt-edu.ma>
2026-05-06 19:34:01 +0100

    login and register

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,16 +1,22 @@
 import { Routes, Route } from 'react-router-dom';
 import MainLayout from '../layout/MainLayout';
 import Dashboard from '../pages/Dashboard';
-
+import Login from '../pages/Login';
+import Register from '../pages/Register';
+import ProtectedRoutes from './ProtectedRoutes';
 // صفحة مؤقتة للسلعة باش نتيستيو
 const Products = () => <div className="p-6 text-2xl font-bold">Gestion des Produits 📦</div>;
 
 const AppRoutes = () => {
   return (
     <Routes>
-      <Route path="/" element={<MainLayout />}>
-        <Route index element={<Dashboard />} />
-        <Route path="products" element={<Products />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route element={<ProtectedRoutes />}>
+        <Route path="/" element={<MainLayout />}>
+          <Route index element={<Dashboard />} />
+          <Route path="products" element={<Products />} />
+        </Route>
       </Route>
     </Routes>
   );
